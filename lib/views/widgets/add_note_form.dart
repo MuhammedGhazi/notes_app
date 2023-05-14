9f6ba6cb93f3aedaafdf2771a7f0fe6c09b85dff
MuhammedGhazi <muhamedghazi79@gmail.com>
2023-05-14 15:35:50 +0300

    add add add

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hint: "Title",
+            onSave: (p0) {
+              title = p0;
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onSave: (p0) {
+              subTitle = p0;
+            },
+            hint: "content",
+            maxlines: 5,
+          ),
+          const SizedBox(
+            height: 36,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
